38121ebaceabb60f1fc23ea912cac32d817e86a1
yeram-lim <yeramyeye@gmail.com>
2022-05-30 21:09:13 +0900

    ✨[FEAT] 질문 게시판 리스트 보는 기능

--- ShareAbroad/urls.py
+++ ShareAbroad/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('QnA/', include('QnA.urls')),
-    path('users/', include('users.urls')),
-    path('information/', include('information.urls')),
-    path('review/', include('review.urls')),
+    path('QnA/', include('QnA.urls', namespace="QnA")),
+    path('users/', include('users.urls', namespace="users")),
+    path('information/', include('information.urls', namespace="information")),
+    path('review/', include('review.urls', namespace="review")),
 ]
