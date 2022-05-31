7e3bf4a3138d4f3780dbf3633cb493a0abda6ff0
yeram-lim <yeramyeye@gmail.com>
2022-05-31 18:48:24 +0900

    ✅[CHORE] 주석 수정
    ##### 제목은 최대 50 글자까지만 입력 ############## -> |
    # 본문은 위에 작성
    ######## 본문은 한 줄에 최대 72 글자까지만 입력 ########################### -> |
    # 꼬릿말은 아래에 작성: ex) #이슈 번호
    # --- COMMIT END ---
    #- ✅[CHORE] : 코드 수정, 내부 파일 수정
    #- ✨[FEAT] : 새로운 기능 구현
    #- ➕[ADD] : Feat 이외의 부수적인 코드 추가, 라이브러리 추가, 새로운 파일 생성 시
    #- 🍱[ADD] : 에셋 추가
    #- 🚑️[HOTFIX] : issue나, QA에서 급한 버그 수정에 사용
    #- 🔨[FIX] : 버그, 오류 해결
    #- ⚰️ [DEL] : 쓸모없는 코드 삭제
    #- 📝[DOCS] : README나 WIKI 등의 문서 개정
    #- 💄[MOD] : storyboard 파일,UI 수정한 경우
    #- ✏️ [CORRECT] : 주로 문법의 오류나 타입의 변경, 이름 변경 등에 사용합니다.
    #- 🚚[MOVE] : 프로젝트 내 파일이나 코드의 이동
    #- ⏪️[RENAME] : 파일 이름 변경이 있을 때 사용합니다.
    #- ⚡️[IMPROVE] : 향상이 있을 때 사용합니다.
    #- ♻️[REFACTOR] : 전면 수정이 있을 때 사용합니다
    #- 🔀[MERGE]: 다른브렌치를 merge 할 때 사용합니다.
    # ------------------
    #     제목 첫 글자를 대문자로
    #     제목은 명령문으로
    #     제목 끝에 마침표(.) 금지
    #     제목과 본문을 한 줄 띄워 분리하기
    #     본문은 "어떻게" 보다 "무엇을", "왜"를 설명한다.
    #     본문에 여러줄의 메시지를 작성할 땐 "-"로 구분
    # ------------------

--- QnA/views.py
+++ QnA/views.py
@@ -3,18 +3,20 @@ from .models import *
 from users.models import *
 from .forms import *
 
-
+# 질문글 작성 view
 def create_question(request):
-    # POST 요청이면 폼 데이터를 처리한다
+    # POST 요청이면 작성된 질문 폼을 처리한다.
     if request.method == 'POST':
 
-        # 폼 인스턴스를 생성하고 요청에 의한 데이타로 채운다 (binding):
+        # 폼 인스턴스를 생성하고 요청에 의한 데이타로 채운다.
         question_form = CreateQuestionForm(request.POST, request.FILES)
 
-        # 폼이 유효한지 체크한다:
+        # 폼이 유효한지(모든 값이 들어있는지) 체크한다:
         if question_form.is_valid():
             question = question_form.save(commit=False)
+            #작성자를 추가해준다.(이 부분은 후에 로그인 기능 만들고 수정할 예정)
             question.writer = User.objects.get(id=1)
+            #질문 글을 저장한다.
             question.save()
 
             # 새로운 URL로 보낸다:
@@ -22,7 +24,7 @@ def create_question(request):
 
     # GET 요청 (혹은 다른 메소드)이면 기본 폼을 생성한다.
     else:
-        question_form = CreateQuestionForm()
+        question_form = CreateQuestionForm() #forms.py에 만들어두고 가져옴
     ctx = {
         'question_form': question_form,
     }
@@ -34,6 +36,7 @@ def create_answer(request):
     return render(request, template_name='QnA/answerCreate.html')
 
 
+# 질문글 하나만 보는 view
 def question_detail(request, pk):
     question = QuestionPost.objects.get(id=pk)
 
@@ -43,6 +46,7 @@ def question_detail(request, pk):
     return render(request, template_name='QnA/detail.html', context=ctx)
 
 
+# 질문들 리스트 게시판 보는 view
 def question_list(request):
     questions = QuestionPost.objects.all()
 
