6ae4d417309d1958bf48d46066dec4db36cde91f
blosson <mh.blosson@gmail.com>
2022-05-31 22:30:11 +0900

    gazua

--- ShareAbroad/urls.py
+++ ShareAbroad/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views # 민혁추가
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('users/', include('users.urls', namespace="users")),
     path('information/', include('information.urls', namespace="information")),
     path('review/', include('review.urls', namespace="review")),
+    path('', views.home), #민혁추가
 ]
