cdfb6b143fa92288aaf378ef064a13955d05d135
blosson <mh.blosson@gmail.com>
2022-06-02 16:26:50 +0900

    test1

--- review/views.py
+++ review/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 
 def review(request):
     return render(request, 'review/review.html')
+
