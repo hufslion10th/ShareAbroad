a3dedc042c4faf2b99828ea7e7297296ccd0d01e
blosson <mh.blosson@gmail.com>
2022-06-02 18:00:03 +0900

    review2

--- review/admin.py
+++ review/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
+from .models import *
 
+@admin.register(ReviewPost)
+class ReviewPostAdmin(admin.ModelAdmin):
+    ordering = ['created_at']
+    search_fields = ['writer']
+    list_display = ['title', 'writer', 'content', 'category', 'like']
+    list_filter = ('writer',)
--- review/urls.py
+++ review/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.static import static
 app_name = 'review'
 
 urlpatterns = [
-    path('', views.review),
-    path('create-R', views.create_review, name='create-R'),
+    path('', views.review_list, name='review-list'),
+    path('create', views.create_review, name='create-R'),
     path('list', views.review_list, name='review-list'),
     path('detail/<int:pk>', views.review_detail, name='review-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
