bd1a17f8dffbf574d2fcb5f379c10306311baaa0
yeram-lim <yeramyeye@gmail.com>
2022-06-07 02:16:11 +0900

    ✅[CHORE] main page 후기 페이지로 연결

--- ShareAbroad/views.py
+++ ShareAbroad/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from review.models import *
 
 def home(request):  # 민혁추가함. 
-    return render(request, 'layout.html')
+    reviews = ReviewPost.objects.all()
+
+    ctx = {
+        "reviews": reviews,
+        "extra_sorting": ['최신 순', '조회 순', '공감 많은 순', '댓글 많은 순'],
+    }
+    return render(request, template_name='review/list.html', context=ctx)
