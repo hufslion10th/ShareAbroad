b93221be09b55ae53f06bfb1587329bc48bd1fff
yeram-lim <yeramyeye@gmail.com>
2022-06-06 20:48:44 +0900

    ⚰️ [DEL] 댓글 기능 삭제

--- QnA/urls.py
+++ QnA/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('create-Q', views.create_question, name='create-Q'),
     path('create-A/<int:pk>', views.create_answer, name='create-A'),
     path('create-Qcomment-ajax/<int:pk>', views.create_comment_of_Q, name='create_comment_of_Q'),
-    # path('create-Acomment-ajax/<int:pk>', views.create_comment_of_A, name='create_comment_of_A'),
     path('list', views.question_list, name='QnA-list'),
     path('detail/<int:pk>', views.question_detail, name='QnA-detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
